b7668fc152748bad62ac69b51df7646241852e4c
tom356@gmail.com <tom356@gmail.com>
2019-01-04 14:40:01 +0100

    Added PrettyPrinter interface

---
+++ PrettyPrintingServer/src/main/java/com/utpcolab/lab/prettyprintingserver/PrettyPrinter.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.utpcolab.lab.prettyprintingserver;
+
+/**
+ *
+ * @author Tomek
+ */
+public interface PrettyPrinter {
+    public String prettyPrint(String source);
+}

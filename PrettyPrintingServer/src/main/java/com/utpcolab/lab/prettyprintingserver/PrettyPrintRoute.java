33cd31d18cadc43ba8863767b7bf2706d29c592a
tom356@gmail.com <tom356@gmail.com>
2019-01-04 14:06:36 +0100

    Added new Route class for pretty print API

---
+++ PrettyPrintingServer/src/main/java/com/utpcolab/lab/prettyprintingserver/PrettyPrintRoute.java
@@ -0,0 +1,18 @@
+package com.utpcolab.lab.prettyprintingserver;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+/**
+ *
+ * @author Tomek
+ */
+public class PrettyPrintRoute implements Route{
+
+    @Override
+    public Object handle(Request rqst, Response rspns) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}

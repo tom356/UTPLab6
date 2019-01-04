18b68df7ea4dfb973f817600b618e6f72dd1e2fa
tom356@gmail.com <tom356@gmail.com>
2019-01-04 15:04:21 +0100

    Added support for multiple pretty printers

--- PrettyPrintingServer/src/main/java/com/utpcolab/lab/prettyprintingserver/PrettyPrintRoute.java
+++ PrettyPrintingServer/src/main/java/com/utpcolab/lab/prettyprintingserver/PrettyPrintRoute.java
@@ -1,5 +1,7 @@
 package com.utpcolab.lab.prettyprintingserver;
 
+import java.util.HashMap;
+import java.util.Map;
 import spark.Request;
 import spark.Response;
 import spark.Route;
@@ -8,11 +10,19 @@ import spark.Route;
  *
  * @author Tomek
  */
-public class PrettyPrintRoute implements Route{
+public class PrettyPrintRoute implements Route {
 
-    @Override
-    public Object handle(Request rqst, Response rspns) throws Exception {
-        return "Not supported Yet";
+    Map<String, PrettyPrinter> prettyPrinters = new HashMap<String, PrettyPrinter>();
+    PrettyPrintRoute(){
+        prettyPrinters.put("default", new DefaultPrettyPrinter());
     }
-    
+            
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+        PrettyPrinter selectedPrettyPrinter = prettyPrinters.get(request.params(":datatype"));
+        if(selectedPrettyPrinter == null)
+            selectedPrettyPrinter = prettyPrinters.get("default");        
+        return selectedPrettyPrinter.prettyPrint(request.body());
+    }
+
 }

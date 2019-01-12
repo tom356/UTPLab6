f756ba7ad7a7a977acba2f1b86a72cd43dc9b26e
CzerwinskiM <czerwinski.mateusz@outlook.com>
2019-01-12 22:49:48 +0100

    so many changes

--- xml.java
+++ xml.java
@@ -3,3 +3,30 @@ function formatXml(xml) {
     var reg = /(>)(<)(\/*)/g;
     xml = xml.replace(reg, '$1\r\n$2$3');
     var pad = 0;
+    jQuery.each(xml.split('\r\n'), function(index, node) {
+        var indent = 0;
+        if (node.match( /.+<\/\w[^>]*>$/ )) {
+            indent = 0;
+        } else if (node.match( /^<\/\w/ )) {
+            if (pad != 0) {
+                pad -= 1;
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+					

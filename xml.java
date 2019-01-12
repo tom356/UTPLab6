2f1d8887eb1b2d2011439829fc5f96b23134ab0a
Anna Marciniak <fazuszka@gmail.com>
2019-01-12 22:55:36 +0100

    komentarz

--- xml.java
+++ xml.java
@@ -1,5 +1,6 @@
 function formatXml(xml) {
-    var formatted = '';
+   // expected output?
+       	var formatted = '';
     var reg = /(>)(<)(\/*)/g;
     xml = xml.replace(reg, '$1\r\n$2$3');
     var pad = 0;

1f70c3d8df887b47b14c13e64550fd4896b09f59
Anna Marciniak <fazuszka@gmail.com>
2019-01-12 22:45:08 +0100

    3 linijka

--- kod.java
+++ kod.java
@@ -5,7 +5,7 @@ console.log(JSON.stringify([new Number(3), new String('false'), new Boolean(fals
 
 
 console.log(JSON.stringify({ x: [10, undefined, function(){}, Symbol('')] }));
-// expected output: "{"x":[10,null,null,null]}"
+
 
 console.log(JSON.stringify(new Date(2006, 0, 2, 15, 4, 5)));
 // expected output: ""2006-01-02T15:04:05.000Z""

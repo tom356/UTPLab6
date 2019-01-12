028adc486d2517a92c8e1d59161c7493414be5b2
Anna Marciniak <fazuszka@gmail.com>
2019-01-12 23:26:27 +0100

    po bledzie

--- xml.java
+++ xml.java
@@ -2,8 +2,7 @@ function formatXml(xml) {
        	davar ddsaformatted ="22";
 
   	var formatted = '';
-2f1d8887eb1b2d2011439829fc5f96b23134ab0a
-    var reg = /(>)(<)(\/*)/g;
+   var reg = /(>)(<)(\/*)/g;
     xml = xml.replace(reg, '$1\r\n$2$3');
     var pad = 0;
     jQuery.each(xml.split('\r\n'), function(index, node) {

afb79257986388ced2c36585634207aaac2bbd88
CzerwinskiM <czerwinski.mateusz@outlook.com>
2019-01-12 23:29:11 +0100

    końcowa wersja pliku xml.java

--- xml.java
+++ xml.java
@@ -1,8 +1,6 @@
 function formatXml(xml) {
-       	davar ddsaformatted ="22";
-
-  	var formatted = '';
-   var reg = /(>)(<)(\/*)/g;
+    var formatted = '';
+    var reg = /(>)(<)(\/*)/g;
     xml = xml.replace(reg, '$1\r\n$2$3');
     var pad = 0;
     jQuery.each(xml.split('\r\n'), function(index, node) {
@@ -13,6 +11,23 @@ function formatXml(xml) {
             if (pad != 0) {
                 pad -= 1;
             }
+        } else if (node.match( /^<\w[^>]*[^\/]>.*$/ )) {
+            indent = 1;
+        } else {
+            indent = 0;
+        }
+
+        var padding = '';
+        for (var i = 0; i < pad; i++) {
+            padding += '  ';
+        }
+
+        formatted += padding + node + '\r\n';
+        pad += indent;
+    });
+
+    return formatted;
+}
 
 
 
